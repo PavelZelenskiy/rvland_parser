48666d2b76b8b669e4c2e5ba0cdc77de4cc7f310
Pavel Zelenskiy <p.z.box@mail.ru>
2023-10-31 22:26:15 +0300

    реализован постраничный обход + добавлены поля в описание кемпингов: телефон, сайт, особенности, отзывы

--- index.js
+++ index.js
@@ -3,52 +3,68 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://rvland.ru/campings/region/all/");
 
-  let urlArr = await page.evaluate(() => {
-    let url = Array.from(document.querySelectorAll("h2 a"), (el) =>
-      el.getAttribute("href")
-    );
-    return url;
-  });
+  let paginationPage = 1;
+  let url = `https://rvland.ru/campings/region/all/page/${paginationPage}/`;
+  let urlArr = [];
+
+  do {
+    await page.goto(url);
+
+    let campUrls = await page.evaluate(() => {
+      let campUrl = Array.from(document.querySelectorAll("h2 a"), (el) =>
+        el.getAttribute("href")
+      );
+      return campUrl;
+    });
+
+    urlArr.push(campUrls);
+
+    paginationPage = paginationPage + 1;
+    url = `https://rvland.ru/campings/region/all/page/${paginationPage}/`;
+  } while (paginationPage <= 34);
+
+  urlArr = urlArr.flat();
 
   //   console.log(urlArr);
+
+  let camps = [];
 
   for (let el of urlArr) {
     await page.goto(el);
 
-    let campSite = new Object();
+    let camp = new Object();
 
-    campSite.title = await page.evaluate(() => {
+    camp.title = await page.evaluate(() => {
       let title = document.querySelector("h1").innerText;
       return title;
     });
-    // campSite.shortDesc = await page.evaluate(() => {
-    //   let shortDesc = document.querySelector("div.desc.txt_18_24").innerText;
-    //   return shortDesc;
-    // });
-    // campSite.desc = await page.evaluate(() => {
-    //   let desc = document.querySelector("div.format_mce").innerText;
-    //   return desc;
-    // });
-    // campSite.photos = await page.evaluate(() => {
-    //   let photos = Array.from(
-    //     document.querySelectorAll("div.gallery a"),
-    //     (el) => el.getAttribute("href")
-    //   );
-    //   return photos;
-    // });
-    // campSite.adress = await page.evaluate(() => {
-    //   let adress = document.querySelector("div.method span").innerText;
-    //   return adress;
-    // });
-    // campSite.coords = await page.evaluate(() => {
-    //   let coords = document.querySelector(
-    //     "div.method span.notranslate"
-    //   ).innerText;
-    //   return coords;
-    // });
-    campSite.tel = await page.evaluate(() => {
+    camp.shortDesc = await page.evaluate(() => {
+      let shortDesc = document.querySelector("div.desc.txt_18_24").innerText;
+      return shortDesc;
+    });
+    camp.desc = await page.evaluate(() => {
+      let desc = document.querySelector("div.format_mce").innerText;
+      return desc;
+    });
+    camp.photos = await page.evaluate(() => {
+      let photos = Array.from(
+        document.querySelectorAll("div.gallery a"),
+        (el) => el.getAttribute("href")
+      );
+      return photos;
+    });
+    camp.adress = await page.evaluate(() => {
+      let adress = document.querySelector("div.method span").innerText;
+      return adress;
+    });
+    camp.coords = await page.evaluate(() => {
+      let coords = document.querySelector(
+        "div.method span.notranslate"
+      ).innerText;
+      return coords;
+    });
+    camp.tel = await page.evaluate(() => {
       for (const a of document.querySelectorAll("a")) {
         if (a.textContent.includes("+7")) {
           let tel = a.textContent;
@@ -56,20 +72,33 @@ const puppeteer = require("puppeteer");
         }
       }
     });
-    campSite.email = await page.evaluate(() => {
+    camp.contact = await page.evaluate(() => {
       for (const a of document.querySelectorAll("span.notranslate a")) {
         if (a.textContent.includes(".ru")) {
-          let email = a.textContent;
-          return email;
+          let contact = a.textContent;
+          return contact;
         }
       }
     });
+    camp.tags = await page.evaluate(() => {
+      let tags = Array.from(
+        document.querySelectorAll("div.item"),
+        (el) => el.innerText
+      );
+      return tags;
+    });
+    camp.reviews = await page.evaluate(() => {
+      let reviews = Array.from(
+        document.querySelectorAll("div.item_review"),
+        (el) => el.innerText
+      );
+      return reviews;
+    });
 
-    // campSite.options
-    console.log(campSite);
+    camps.push(camp);
   }
 
-  //   console.log(campSite);
+  console.log(camps);
 
   await browser.close();
 })();
